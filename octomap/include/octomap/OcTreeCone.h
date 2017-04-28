ff5fc8ccb7f7daf10365fbb62744fa758e9694ee
Ross Barnowski <rossbar@berkeley.edu>
2017-04-27 17:24:30 -0700

    Copied OcTreeStamped to create a prototype for cone data.
    OcTree that has an extra double, cv_prob, for storing cone voxel
    probability.
    
    Build successful.

---
+++ octomap/include/octomap/OcTreeCone.h
@@ -0,0 +1,130 @@
+/*
+ * OctoMap - An Efficient Probabilistic 3D Mapping Framework Based on Octrees
+ * http://octomap.github.com/
+ *
+ * Copyright (c) 2009-2013, K.M. Wurm and A. Hornung, University of Freiburg
+ * All rights reserved.
+ * License: New BSD
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Freiburg nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef OCTOMAP_OCTREE_STAMPED_H
+#define OCTOMAP_OCTREE_STAMPED_H
+
+
+#include <octomap/OcTreeNode.h>
+#include <octomap/OccupancyOcTreeBase.h>
+#include <ctime>
+
+namespace octomap {
+  
+  // node definition
+  class OcTreeNodeCone : public OcTreeNode {    
+
+  public:
+    OcTreeNodeCone() : OcTreeNode(), cv_prob(0) {}
+
+    // copy constructor
+    OcTreeNodeCone(const OcTreeNodeCone& rhs) : OcTreeNode(rhs), cv_prob(rhs.cv_prob) {}
+
+    bool operator==(const OcTreeNodeCone& rhs) const{
+      return (rhs.value == value && rhs.cv_prob == cv_prob);
+    }
+    
+    void copyData(const OcTreeNodeCone& from){
+      OcTreeNode::copyData(from);
+      cv_prob = from.getConeVoxelProbability();
+    }
+      
+    // timestamp
+    inline unsigned int getConeVoxelProbability() const { return cv_prob; }
+    //TODO: Placeholder method for computing cone voxel probability
+    inline void updateConeVoxelProbability() { cv_prob = cv_prob + 1;}
+    inline void setConeVoxelProbability(double cv) {this->cv_prob = cv_prob; }
+
+    // update occupancy and timesteps of inner nodes 
+    inline void updateOccupancyChildren() {      
+      this->setLogOdds(this->getMaxChildLogOdds());  // conservative
+      updateConeVoxelProbability();
+    }
+
+  protected:
+    double cv_prob;
+  };
+
+
+  // tree definition
+  class OcTreeCone : public OccupancyOcTreeBase <OcTreeNodeCone> {    
+
+  public:
+    /// Default constructor, sets resolution of leafs
+	OcTreeCone(double resolution);
+      
+    /// virtual constructor: creates a new object of same type
+    /// (Covariant return type requires an up-to-date compiler)
+    OcTreeCone* create() const {return new OcTreeCone(resolution); }
+
+    std::string getTreeType() const {return "OcTreeCone";}
+
+    //! \return timestamp of last update
+//    unsigned int getLastUpdateTime();
+//
+//    void degradeOutdatedNodes(unsigned int time_thres);
+//    
+//    virtual void updateNodeLogOdds(OcTreeNodeCone* node, const float& update) const;
+//    void integrateMissNoTime(OcTreeNodeCone* node) const;
+
+  protected:
+    /**
+     * Static member object which ensures that this OcTree's prototype
+     * ends up in the classIDMapping only once. You need this as a 
+     * static member in any derived octree class in order to read .ot
+     * files through the AbstractOcTree factory. You should also call
+     * ensureLinking() once from the constructor.
+     */
+    class StaticMemberInitializer{
+    public:
+      StaticMemberInitializer() {
+        OcTreeCone* tree = new OcTreeCone(0.1);
+        tree->clearKeyRays();
+        AbstractOcTree::registerTreeType(tree);
+      }
+
+      /**
+      * Dummy function to ensure that MSVC does not drop the
+      * StaticMemberInitializer, causing this tree failing to register.
+      * Needs to be called from the constructor of this octree.
+      */
+      void ensureLinking() {};
+    };
+    /// to ensure static initialization (only once)
+    static StaticMemberInitializer ocTreeConeMemberInit;
+    
+  };
+
+} // end namespace
+
+#endif
